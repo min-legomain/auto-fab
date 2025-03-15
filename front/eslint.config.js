64e709dc3d395b6ae1c0550251168b522adea9ae
min-legomain <gumink0174@gmain.com>
2025-03-15 22:45:50 +0900

    vite以降

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -1,37 +1,38 @@
-import eslintPluginReact from 'eslint-plugin-react';
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
-import eslintPluginSecurity from 'eslint-plugin-security';
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
   {
-    ignores: ['node_modules/', 'dist/'],
-  },
-  {
-    files: ['src/**/*.{ts,tsx,js,jsx}'],
+    ignores: ["dist"], // ESLint を適用しないディレクトリ
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: 2020,
+      globals: globals.browser,
       parser: tsParser,
+      sourceType: "module",
     },
     plugins: {
-      '@typescript-eslint': tsPlugin,
-      react: eslintPluginReact,
-      'react-hooks': eslintPluginReactHooks,
-      security: eslintPluginSecurity,
-      prettier: eslintPluginPrettier,
+      react: react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@typescript-eslint": tseslint,
     },
     rules: {
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'security/detect-object-injection': 'warn',
-      'prettier/prettier': 'error',
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/react-in-jsx-scope": "off",
     },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
   },
--- server/main.go
+++ server/main.go
@@ -14,7 +14,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
