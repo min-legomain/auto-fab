1f10390d45262831b14049799b7fd9a37e8e1aec
min-legomain <gumink0174@gmain.com>
2025-03-16 00:41:50 +0900

    仮UI実装

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,19 @@
 import ApiButton from "./components/ApiButton";
 import SyncForm from "./components/SyncForm";
+import CardListTable from "./components/CardListTable";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <ApiButton />
-      <SyncForm />
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-6 p-6 bg-gray-100 dark:bg-gray-900">
+      <div className="w-full max-w-md">
+        <ApiButton />
+      </div>
+      <div className="w-full max-w-md">
+        <SyncForm />
+      </div>
+      <div className="w-full max-w-3xl">
+        <CardListTable />
+      </div>
     </div>
   );
 }
